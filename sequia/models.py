f72319ea3d52239a8d874057165e30ce572f2677
crocha09.09 <crocha09.09@329be340-cbcb-11de-9a23-210ccc102f2f>
2009-11-07 19:58:51 +0000

    second commit

---
+++ sequia/models.py
@@ -0,0 +1,135 @@
+ # -*- coding: UTF-8 -*-
+ 
+from django.db import models
+from lugar.models import Comunidad
+import datetime
+from django.contrib.contenttypes import generic
+from django.contrib.contenttypes.models import ContentType
+
+#Empieza las definiciones de las tablas de la base de datos
+class Organizacion(models.Model):
+    nombre = models.CharField(max_length=200)
+    
+    class Meta:
+        verbose_name_plural="Organizacion"
+        
+    def __unicode__(self):
+        return self.nombre
+    
+class Entrevistado(models.Model):
+    content_type = models.ForeignKey(ContentType)
+    object_id = models.IntegerField(db_index=True)
+    content_object = generic.GenericForeignKey()
+    nombre = models.CharField(max_length=200)
+    comunidad = models.ForeignKey(Comunidad)
+    
+    class Meta:
+        verbose_name_plural="Entrevistado"
+    def __unicode__(self):
+        return self.nombre
+
+class Producto(models.Model):
+    nombre = models.CharField(max_length=200)
+    
+    class Meta:
+        verbose_name_plural = "Producto"
+        
+    def __unicode__(self):
+        return self.nombre
+       
+#PRODUCTO_CHOICES=((1,"Maíz"),(2,"Frijol"),(3,"Sorgo"))
+PERDIDA_CHOICES=((1,"Sequia"),(2,"Mala calidad de la semilla"),(3,"Ataque de plagas"))
+class Primera(models.Model):
+    content_type = models.ForeignKey(ContentType)
+    object_id = models.IntegerField(db_index=True)
+    content_object = generic.GenericForeignKey()
+#    producto = models.IntegerField(choices=PRODUCTO_CHOICES)
+    producto = models.ForeignKey(Producto)
+    area_sembrada = models.DecimalField(max_digits=10, decimal_places=2)
+    area_cosechada = models.DecimalField(max_digits=10, decimal_places=2)
+    produccion = models.DecimalField(max_digits=10, decimal_places=2)
+    perdida = models.IntegerField(choices=PERDIDA_CHOICES)
+    
+    class Meta:
+        verbose_name_plural="Primera"
+        
+    def __unicode__(self):
+        return self.get_producto_display()
+    
+class Postrera(models.Model):
+    content_type = models.ForeignKey(ContentType)
+    object_id = models.IntegerField(db_index=True)
+    content_object = generic.GenericForeignKey()
+#    producto = models.IntegerField(choices=PRODUCTO_CHOICES)
+    producto = models.ForeignKey(Producto)
+    area_sembrada = models.DecimalField(max_digits=10, decimal_places=2)
+    area_cosechada = models.DecimalField(max_digits=10, decimal_places=2)
+    produccion = models.DecimalField(max_digits=10, decimal_places=2)
+    perdida = models.IntegerField(choices=PERDIDA_CHOICES)
+    
+    class Meta:
+        verbose_name_plural="Postrera"
+        
+    def __unicode__(self):
+        return self.get_producto_display()
+    
+class Apante(models.Model):
+    content_type = models.ForeignKey(ContentType)
+    object_id = models.IntegerField(db_index=True)
+    content_object = generic.GenericForeignKey()
+#    producto = models.IntegerField(choices=PRODUCTO_CHOICES)
+    producto = models.ForeignKey(Producto)
+    area_sembrada = models.DecimalField(max_digits=10, decimal_places=2)
+    area_cosechada = models.DecimalField(max_digits=10, decimal_places=2)
+    produccion = models.DecimalField(max_digits=10, decimal_places=2)
+    perdida = models.IntegerField(choices=PERDIDA_CHOICES)
+    
+    class Meta:
+        verbose_name_plural="Apante"
+        
+    def __unicode__(self):
+        return self.get_producto_display()
+    
+class Disponibilidad(models.Model):
+    content_type = models.ForeignKey(ContentType)
+    object_id = models.IntegerField(db_index=True)
+    content_object = generic.GenericForeignKey()
+    adultos_casa = models.IntegerField()
+    ninos_casa = models.IntegerField()
+    vacas = models.IntegerField()
+    cerdos = models.IntegerField()
+    gallinas = models.IntegerField()
+    maiz_disponible= models.DecimalField(max_digits=10, decimal_places=2)
+    frijol_disponible = models.DecimalField(max_digits=10, decimal_places=2)
+    sorgo_disponible = models.DecimalField(max_digits=10, decimal_places=2)
+    dinero = models.DecimalField(max_digits=10, decimal_places=2)
+    
+    class Meta:
+        verbose_name_plural = "Disponibilidad"
+    
+class Nutricion(models.Model):
+    content_type = models.ForeignKey(ContentType)
+    object_id = models.IntegerField(db_index=True)
+    content_object = generic.GenericForeignKey()
+    ninos = models.CharField(max_length=50)
+    edad = models.IntegerField()
+    brazalete = models.CharField(max_length=200)
+    
+    class Meta:
+        verbose_name_plural="Nutricion"
+        
+    def __unicode__(self):
+        return self.brazalete
+    
+class Encuesta(models.Model):
+    fecha = models.DateField()
+    colector = models.CharField(max_length=100)
+    organizacion = models.ForeignKey(Organizacion)
+    entrevistado = generic.GenericRelation(Entrevistado)
+    primera = generic.GenericRelation(Primera)
+    postrera = generic.GenericRelation(Postrera)
+    apante = generic.GenericRelation(Apante)
+    disponibilidad = generic.GenericRelation(Disponibilidad)
+    nutricion = generic.GenericRelation(Nutricion)
+    
+
---
+++ settings.py
@@ -0,0 +1,43 @@
+# Django settings for sequias project.
+
+from local_settings import *
+
+#LOGIN_REDIRECT_URL="/"
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+AUTH_PROFILE_MODULE = "profiles.UserProfile"
+DEFAULT_CHARSET = 'utf-8'
+MANAGERS = ADMINS
+DATABASE_ENGINE = 'sqlite3'
+SEARCH_ENGINE = 'sqlite3'
+ACCOUNT_ACTIVATION_DAYS = 7
+SITE_ID = 1
+USE_I18N = True
+ADMIN_MEDIA_PREFIX = '/media/admin/'
+TEMPLATE_LOADERS = (
+    'django.template.loaders.filesystem.load_template_source',
+    'django.template.loaders.app_directories.load_template_source',
+)
+MIDDLEWARE_CLASSES = (
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+)
+
+
+MEDIA_ROOT = os.path.join(SITE_ROOT,'media')
+MEDIA_URL = '/media'
+TEMPLATE_DIRS = (
+SITE_ROOT+"/templates",
+)
+ROOT_URLCONF = 'urls'
+
+INSTALLED_APPS = (
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.sites',
+    'django.contrib.admin',
+    'lugar',
+    'sequia',
+)
