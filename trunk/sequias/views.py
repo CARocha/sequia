da02f38c64c81f3722b2159b2374eb5862139370
crocha09.09 <crocha09.09@329be340-cbcb-11de-9a23-210ccc102f2f>
2009-11-10 06:12:39 +0000

    adding other stuff

--- trunk/sequias/views.py
+++ trunk/sequias/views.py
@@ -1,1 +1,6 @@
-# Create your views here.
+from django.shortcuts import render_to_response
+from sequia.sequias.models import *
+from django.conf import settings
+
+def index(request):
+    return render_to_response('base.html')
