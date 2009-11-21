c709588926e1c543ba6f137abb2490aebffb760c
crocha09.09 <crocha09.09@329be340-cbcb-11de-9a23-210ccc102f2f>
2009-11-21 22:11:19 +0000

    adding first views perdida cosecha

--- sequias/urls.py
+++ sequias/urls.py
@@ -9,6 +9,7 @@ info = {
 
 urlpatterns = patterns('sequia.sequias.views',
     (r'^index/$', 'consultar'),
+    (r'^perdida/$', 'perdidapostrera'),
     (r'^ajax/municipio/(?P<departamento>\d+)/$', 'get_municipios'),
     (r'^ajax/comunidad/(?P<municipio>\d+)/$', 'get_comunidad'),
     (r'^ajax/entrevista/(?P<comunidad>\d+)/$', 'get_entrevista'),
--- sequias/views.py
+++ sequias/views.py
@@ -11,6 +11,9 @@ from decimal import Decimal
 from django.views.generic.simple import direct_to_template
 from django.http import HttpResponse, HttpResponseBadRequest
 from decorators import session_required
+from django.views.decorators.cache import cache_page
+from django.template.loader import get_template
+from django.template import Context
 
 def index(request):
     return render_to_response('base.html')
@@ -60,3 +63,55 @@ def get_entrevista(request, comunidad):
     entrevistados = Entrevistado.objects.filter(comunidad = comunidad )
     lista = [(entrevista.id, entrevista.nombre) for entrevista in entrevistados]
     return HttpResponse(simplejson.dumps(lista), mimetype='application/javascript')
+
+#Vista para la perdida de la cosecha primera
+
+
+def perdidapostrera(request):
+    fecha1=request.session['fecha_inicio']
+    fecha2=request.session['fecha_final']
+    if request.session['comunidad']:
+        com = request.session['comunidad'].id
+        perdida = Encuesta.objects.filter(fecha=fecha1,fecha=fecha2).filter(entrevistado__comunidad__id=com)
+    elif request.session['municipio']:
+        mun = request.session['municipio'].id
+        perdida = Encuesta.objects.filter(fecha=fecha1,fecha=fecha2).filter(entrevistado__comunidad__municipio__id=mun)
+    elif request.session['departamento']:
+        dep = request.session['departamento'].id
+        perdida = Encuesta.objects.filter(fecha=fecha1,fecha=fecha2).filter(entrevistado__comunidad__municipio__departamento__id=dep)
+    elif request.session['entrevistado']:
+        entre = request.session['entrevistado'].id
+        perdida = Encuesta.objects.filter(fecha=fecha1,fecha=fecha2).filter(entrevistado__nombre=entre)
+    else:
+        perdida = Encuesta.objects.all()
+        
+    casos = perdida.count()
+    #TODO: Sumatorias de maiz,frijol,sorgo primera de area_sembrada
+    arroz_sembrada = perdida.filter(primera__producto__id=1).aggregate(Sum('primera__area_sembrada'))
+    frijol_sembrada = perdida.filter(primera__producto__id=2).aggregate(Sum('primera__area_sembrada'))
+    maiz_sembrada = perdida.filter(primera__producto__id=3).aggregate(Sum('primera__area_sembrada'))
+    #TODO: area cosechada
+    arroz_cosechada = perdida.filter(primera__producto__id=1).aggregate(Sum('primera__area_cosechada'))
+    frijol_cosechada = perdida.filter(primera__producto__id=2).aggregate(Sum('primera__area_cosechada'))
+    maiz_cosechada = perdida.filter(primera__producto__id=3).aggregate(Sum('primera__area_cosechada'))
+    #TODO:area perdida
+    arroz_perdida = (arroz_sembrada - arroz_cosechada)
+    frijol_perdida = (frijol_sembrada - frijol_cosechada)
+    maiz_perdida = (maiz_sembrada - maiz_cosechada)
+    #TODO: produccion
+    arroz_produccion = perdida.filter(primera__producto__id=1).aggregate(Sum('primera__produccion'))
+    frijol_produccion = perdida.filter(primera__producto__id=2).aggregate(Sum('primera__produccion'))
+    maiz_produccion = perdida.filter(primera__producto__id=3).aggregate(Sum('primera__produccion'))
+    #TODO: rendimientos
+    arroz_rendi = arroz_produccion / arroz_cosechada
+    frijol_rendi = frijol_produccion / frijol_cosechada
+    maiz_rendi = maiz_produccion / maiz_cosechada
+    
+    return render_to_response("encuesta/perdida.html",{'casos':casos,'arroz_sembrada':arroz_sembrada,
+            'frijol_sembrada':frijol_sembrada,'maiz_sembrada':maiz_sembrada,
+            'arroz_cosechada':arroz_cosechada,'frijol_cosechada':frijol_cosechada,
+            'maiz_cosechada':maiz_cosechada,'arroz_perdida':arroz_perdida,'frijol_perdida':frijol_perdida,
+            'maiz_perdida':maiz_perdida,'arroz_produccion':arroz_produccion,'frijol_produccion':frijol_produccion,
+            'maiz_produccion':maiz_produccion,'arroz_rendi':arroz_rendi,
+            'frijol_rendi':frijol_rendi,'maiz_rendi':maiz_rendi })
+    
