e93163722d66b66178e7ff877e321d6f4cb8c413
crocha09.09 <crocha09.09@329be340-cbcb-11de-9a23-210ccc102f2f>
2009-11-25 03:50:25 +0000

    adding and fix files and views

--- sequias/views.py
+++ sequias/views.py
@@ -15,6 +15,7 @@ from django.views.decorators.cache import cache_page
 from django.template.loader import get_template
 from django.template import Context
 from pygooglechart import PieChart3D, StackedVerticalBarChart
+from utils import grafos
 
 def index(request):
     return render_to_response('base.html')
@@ -293,6 +294,7 @@ def nutricion(request):
     
     return render_to_response("encuesta/nutricion.html", locals())
 
+@session_required
 def grafo_perdida(request):
     fecha1=request.session['fecha_inicio']
     fecha2=request.session['fecha_final']
@@ -323,25 +325,218 @@ def grafo_perdida(request):
     casos = gperdida.count()
     maiz_s = 0
     maiz_c = 0
+    frijol_s = 0
+    frijol_c = 0
+    sorgo_s = 0
+    sorgo_c = 0
+    razon1 = 0
+    razon2 = 0
+    razon3 = 0
+    razon4 = 0
+    razon5 = 0
+    razon6 = 0
+    razon7 = 0
     for encuesta in gperdida:
-     for primera in encuesta.primera.all():
-        maiz_s = primera.area_sembrada + maiz_s
-        maiz_c = primera.area_cosechada + maiz_c
-		
+        for primera in encuesta.primera.filter(producto__id=2):
+            maiz_s = primera.area_sembrada + maiz_s
+            maiz_c = primera.area_cosechada + maiz_c
+    for encuesta in gperdida:
+        for primera in encuesta.primera.filter(producto__id=3):
+            frijol_s = primera.area_sembrada + frijol_s
+            frijol_c = primera.area_cosechada + frijol_c
+    for encuesta in gperdida:
+        for primera in encuesta.primera.filter(producto__id=4):
+            sorgo_s = primera.area_sembrada + sorgo_s
+            sorgo_c = primera.area_cosechada + sorgo_c
+    #Razones de perdida primera
+    for encuesta in gperdida:
+        for primera in encuesta.primera.filter(perdida__id=1):
+            razon1 = primera.perdida.id + razon1
+    for encuesta in gperdida:
+        for primera in encuesta.primera.filter(perdida__id=2):
+            razon2 = primera.perdida.id + razon2
+    for encuesta in gperdida:
+        for primera in encuesta.primera.filter(perdida__id=3):
+            razon3 = primera.perdida.id + razon3
+    #Esto es para postrera
+    maiz_s_P = 0
+    maiz_c_P = 0
+    frijol_s_P = 0
+    frijol_c_P = 0
+    sorgo_s_P = 0
+    sorgo_c_P = 0
+    razon1_p = 0
+    razon2_p = 0
+    razon3_p = 0
+    razon4_p = 0
+    razon5_p = 0
+    razon6_p = 0
+    razon7_p = 0
+    for encuesta in gperdida:
+        for postrera in encuesta.postrera.filter(producto__id=2):
+            maiz_s_P = postrera.area_sembrada + maiz_s_P
+            maiz_c_P = postrera.area_cosechada + maiz_c_P
+    for encuesta in gperdida:
+        for postrera in encuesta.postrera.filter(producto__id=3):
+            frijol_s_P = postrera.area_sembrada + frijol_s_P
+            frijol_c_P = postrera.area_cosechada + frijol_c_P
+    for encuesta in gperdida:
+        for postrera in encuesta.postrera.filter(producto__id=4):
+            sorgo_s_P = postrera.area_sembrada + sorgo_s_P
+            sorgo_c_P = postrera.area_cosechada + sorgo_c_P
+    #Razones de perdidas postrera
+    for encuesta in gperdida:
+        for postrera in encuesta.postrera.filter(perdida__id=1):
+            razon1_p = primera.perdida.id + razon1_p
+    for encuesta in gperdida:
+        for postrera in encuesta.postrera.filter(perdida__id=2):
+            razon2_p = postrera.perdida.id + razon2_p
+    for encuesta in gperdida:
+        for postrera in encuesta.postrera.filter(perdida__id=3):
+            razon3_p = postrera.perdida.id + razon3_p
+
+    #Calculacion de las perdidas de primera 
     resta = maiz_s - maiz_c
-    
+    resta_f = frijol_s - frijol_c
+    resta_s = sorgo_s - sorgo_c
+    #Calculacion de las perdidas de postrera
+    resta_P = maiz_s_P - maiz_c_P
+    resta_f_P = frijol_s_P - frijol_c_P
+    resta_s_P = sorgo_s_P - sorgo_c_P
+    #Calculacion de los porcentajes de primera
     p_c = (float(maiz_c)) *100
     p_p = (float(resta)) *100
-
-    graph = PieChart3D(400,200)
-    graph.set_colours([ 'FFBC13','22A410',])
-    graph.add_data([p_c,p_p])
-    graph.set_legend(['area cosechada','area perdidad'])
-#    porcentajes = saca_porcentajes([data[1] for data in resultados])
-    graph.set_pie_labels(['p_c','p_p'])
-    graph.set_legend_position("b")
-    graph.set_title("Maiz")
-    url = graph.get_url()
-    print url
     
-    return render_to_response("encuesta/grafos.html",{ 'url':url })
+    p_c_f = (float(frijol_c)) *100
+    p_p_f = (float(resta_f)) *100
+    
+    p_c_s = (float(sorgo_c)) *100
+    p_p_s = (float(resta_s)) *100
+    
+    p_razon1= (float(razon1)) *100
+    p_razon2= (float(razon2)) *100
+    p_razon3= (float(razon3)) *100
+    #Calculacion de los porcentajes de la Postrera
+    p_c_P = (float(maiz_c_P)) *100
+    p_p_P = (float(resta_P)) *100
+    
+    p_c_f_P = (float(frijol_c_P)) *100
+    p_p_f_P = (float(resta_f_P)) *100
+    
+    p_c_s_P = (float(sorgo_c_P)) *100
+    p_p_s_P = (float(resta_s_P)) *100
+    
+    p_razon1_p= (float(razon1_p)) *100
+    p_razon2_p= (float(razon2_p)) *100
+    p_razon3_p= (float(razon3_p)) *100
+    #envios de los datos a utils solo primera
+    lista = [p_c,p_p]
+    lista1 = [p_c_f,p_p_f]
+    lista2 = [p_c_s,p_p_s]
+    lista3 = [p_razon1,p_razon2,p_razon3]
+    legends = ['Area Cosechada','Area Perdida']
+    legends1 = ['Area Cosechada','Area Perdida']
+    legends2 = ['Area Cosechada','Area Perdida']
+    legends3 = ['Sequia','Mala semilla','plaga']
+    mensa = "Grafico Maiz"
+    mensa1 = "Grafico Frijol"
+    mensa2 = "Grafico Sorgo"
+    mensa3 = "Grafico razones de perdida"
+    #envios de los datos a utils solo para postrera
+    lista_P = [p_c_P,p_p_P]
+    lista1_P = [p_c_f_P,p_p_f_P]
+    lista2_P = [p_c_s_P,p_p_s_P]
+    lista3_P = [p_razon1_p,p_razon2_p,p_razon3_p]
+    legends_P = ['Area Cosechada','Area Perdida']
+    legends1_P = ['Area Cosechada','Area Perdida']
+    legends2_P = ['Area Cosechada','Area Perdida']
+    legends3_P = ['Sequia','Mala semilla','plaga']
+    mensa_P = "Grafico Maiz"
+    mensa1_P = "Grafico Frijol"
+    mensa2_P = "Grafico Sorgo"
+    mensa3_P = "Grafico razones de perdida"
+    #Envios de las url solo para primera
+    a = grafos.make_graph(lista, legends, mensa, return_json=False)
+    b = grafos.make_graph(lista1, legends1, mensa1, return_json=False)
+    c = grafos.make_graph(lista2, legends2, mensa2, return_json=False)
+    g = grafos.make_graph(lista3, legends3, mensa3, return_json=False)
+    #Envios de las url solo para postrera
+    d = grafos.make_graph(lista_P, legends_P, mensa_P, return_json=False)
+    e = grafos.make_graph(lista1_P, legends1_P, mensa1_P, return_json=False)
+    f = grafos.make_graph(lista2_P, legends2_P, mensa2_P, return_json=False)
+    h = grafos.make_graph(lista3_P, legends3_P, mensa3_P, return_json=False)
+    
+    return render_to_response("encuesta/grafos.html",{ 'url':a, 'url1':b, 'url2':c,
+                                                       'url3':d, 'url4':e, 'url5':f, 
+                                                       'url6':g, 'url7':h, 'casos':casos })
+                                                       
+@session_required
+def grafo_nutricion(request):
+    fecha1=request.session['fecha_inicio']
+    fecha2=request.session['fecha_final']
+    if request.session['comunidad']:
+        com = request.session['comunidad'].id
+        if request.session['entrevistado'] !=None:
+            gnutri = Encuesta.objects.filter(fecha__range=(fecha1,fecha2)).filter(entrevistado__nombre=request.session['entrevistado'])
+        else:
+            gnutri = Encuesta.objects.filter(fecha__range=(fecha1,fecha2)).filter(entrevistado__comunidad__id=com)
+    elif request.session['municipio']:
+        mun = request.session['municipio'].id
+        if request.session['entrevistado'] !=None:
+            gnutri = Encuesta.objects.filter(fecha__range=(fecha1,fecha2)).filter(entrevistado__nombre=request.session['entrevistado'])
+        else:
+            gnutri = Encuesta.objects.filter(fecha__range=(fecha1,fecha2)).filter(entrevistado__comunidad__municipio__id=mun)
+    elif request.session['departamento']:
+        dep = request.session['departamento'].id
+        if request.session['entrevistado'] !=None:
+            gnutri = Encuesta.objects.filter(fecha__range=(fecha1,fecha2)).filter(entrevistado__nombre=request.session['entrevistado'])
+        else:
+            gnutri = Encuesta.objects.filter(fecha__range=(fecha1,fecha2)).filter(entrevistado__comunidad__municipio__departamento__id=dep)
+    elif request.session['entrevistado']:
+        entre = request.session['entrevistado']
+        gnutri = Encuesta.objects.filter(fecha__range=(fecha1,fecha2)).filter(entrevistado__nombre=entre)
+    else:
+        gnutri = Encuesta.objects.all()
+        
+    casos = gnutri.count()
+    normal_v = 0
+    desnutrido_v = 0
+    riesgo_v = 0
+    nosabe_v = 0
+    normal_m = 0
+    desnutrido_m = 0
+    riesgo_m = 0
+    nosabe_m = 0
+    # solo para niños
+    for encuesta in gnutri:
+        for nutricion in encuesta.nutricion.filter(ninos__contains="ninos").filter(brazalete__id=1):
+            normal_v = nutricion.brazalete.id + normal_v
+        for nutricion in encuesta.nutricion.filter(ninos__contains="ninos").filter(brazalete__id=3):
+            desnutrido_v = nutricion.brazalete.id + desnutrido_v
+        for nutricion in encuesta.nutricion.filter(ninos__contains="ninos").filter(brazalete__id=4):
+            riesgo_v = nutricion.brazalete.id + riesgo_v
+        for nutricion in encuesta.nutricion.filter(ninos__contains="ninos").filter(brazalete__id=5):
+            nosabe_v = nutricion.brazalete.id + normal_v
+    # solo para niñas
+    for encuesta in gnutri:
+        for nutricion in encuesta.nutricion.filter(ninos__contains="ninas").filter(brazalete__id=1):
+            normal_m = nutricion.brazalete.id + normal_m
+        for nutricion in encuesta.nutricion.filter(ninos__contains="ninas").filter(brazalete__id=3):
+            desnutrido_m = nutricion.brazalete.id + desnutrido_m
+        for nutricion in encuesta.nutricion.filter(ninos__contains="ninas").filter(brazalete__id=4):
+            riesgo_m = nutricion.brazalete.id + riesgo_m
+        for nutricion in encuesta.nutricion.filter(ninos__contains="ninas").filter(brazalete__id=5):
+            nosabe_m = nutricion.brazalete.id + normal_m
+            
+    #mandar los datos al utils solo de niños y niñas
+    lista1 = [normal_v,desnutrido_v,riesgo_v,nosabe_v]
+    lista2 = [normal_m,desnutrido_m,riesgo_m,nosabe_m]
+    legends1 = ['Normal','Desnutrido','Riesgo desnutricion','No sabe']
+    legends2 = ['Normal','Desnutrido','Riesgo desnutricion','No sabe']
+    mensa1 = "Grafico Nutrición Niños"
+    mensa2 = "Grafico Nutrición Niñas"
+    #los link :)
+    a = grafos.make_graph(lista1,legends1,mensa1,return_json=False)
+    b = grafos.make_graph(lista2,legends2,mensa2,return_json=False)
+    
+    return render_to_response("encuesta/grafo_nutricion.html",{'url':a,'url1':b })
